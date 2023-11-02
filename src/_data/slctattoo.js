cb25f67c56eaa9ab8352f5797f4f4585c9b07858
Kevin Thomson <kevinbthomson@gmail.com>
2023-11-01 20:52:19 -0600

    2024 date changes

--- src/_data/slctattoo.js
+++ src/_data/slctattoo.js
@@ -1,21 +1,26 @@
 module.exports = function () {
   return {
     ticketLink:
-      'https://www.24tix.com/event/1477390210/salt-lake-city-tattoo-expo',
+      'https://www.24tix.com/event/2063067077/salt-lake-city-tattoo-expo-2024',
+    hotelLink:
+      'https://book.passkey.com/gt/219503829?gtid=cb28fa2bd368f5a0d84191aa731316af',
+    facebookLink:
+      'https://www.facebook.com/people/SLC-Tattoo-Expo/100083123352663/?mibextid=LQQJ4d',
+    instagramLink: 'https://instagram.com/slctattooexpo',
     dates: [
       {
         day: 'Friday',
-        date: '17',
+        date: '29',
         times: '3pm - 11pm',
       },
       {
         day: 'Saturday',
-        date: '18',
+        date: '30',
         times: '12pm - 11pm',
       },
       {
         day: 'Sunday',
-        date: '19',
+        date: '31',
         times: '12pm - 8pm',
       },
     ],
