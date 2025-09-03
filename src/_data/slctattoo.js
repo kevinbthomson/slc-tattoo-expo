413ab10d00f062810fcb28bf69bfac3ff01b213e
Kevin Thomson <kevinbthomson@gmail.com>
2025-09-02 18:30:31 -0600

    update 2026 dates

--- src/_data/slctattoo.js
+++ src/_data/slctattoo.js
@@ -1,24 +1,24 @@
 module.exports = {
-  ticketLink: 'https://www.24tix.com/event/47726752371',
+  ticketLink: 'https://www.24tix.com/event/58398255072',
   hotelLink: 'https://book.passkey.com/event/50915178/owner/1440/home',
   facebookLink:
     'https://www.facebook.com/people/SLC-Tattoo-Expo/100083123352663/?mibextid=LQQJ4d',
   instagramLink: 'https://instagram.com/slctattooexpo',
-  year: 2025,
+  year: 2026,
   dates: [
     {
       day: 'Friday',
-      date: '7',
+      date: '6',
       times: '3pm - 11pm',
     },
     {
       day: 'Saturday',
-      date: '8',
+      date: '7',
       times: '12pm - 11pm',
     },
     {
       day: 'Sunday',
-      date: '9',
+      date: '8',
       times: '12pm - 8pm',
     },
   ],
